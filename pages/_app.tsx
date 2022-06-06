6b035b365ed6c761920de21c6f4e5d263966bc0e
Matthias Nannt <mail@matthiasnannt.com>
2022-06-07 03:01:31 +0900

    add tracking with plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        {process.env.NODE_ENV === "production" && (
+          <script
+            defer
+            data-domain="salmon.stackocean.com"
+            src="https://plausible.stackocean.com/js/plausible.js"
+          />
+        )}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
